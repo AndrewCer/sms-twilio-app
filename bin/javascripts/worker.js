e51c01bec4ecae4c18412b6fdacae8f372e5ef7e
Andrew Cer <cer.andrew@gmail.com>
2015-07-17 16:44:37 -0600

    twilio worker test

--- bin/javascripts/worker.js
+++ bin/javascripts/worker.js
@@ -1,6 +1,10 @@
-function sayHello() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/texty-text', false);
-  xhr.send(null);
+// var app = require('../app');
+
+function textingWorker() {
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', '/texty-text', false);
+  // xhr.send(null);
+  console.log('TEST TEST TEST');
+  window.open('/texty-text');
 }
-sayHello();
+textingWorker();
